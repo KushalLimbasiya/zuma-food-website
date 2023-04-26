81c7960dc5d00b6dd3b2d4087cc7b21b03751782
Kushal <kushallimbasiya@gmail.com>
2023-04-26 21:20:06 +0530

    changes on contact page

--- src/components/page/layout/contact.js
+++ src/components/page/layout/contact.js
@@ -4,8 +4,6 @@ import '../../style/main.css';
 const Contact = () => {
   return (
         <div>
-        <title>Contact Form 06</title>
-        <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <link href="https://fonts.googleapis.com/css?family=Roboto:400,100,300,700" rel="stylesheet" type="text/css" />
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
