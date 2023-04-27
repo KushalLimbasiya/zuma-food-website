0256fb8e662ccbb22b67c09227a97b0d88623253
Kushal <kushallimbasiya@gmail.com>
2023-04-27 18:37:44 +0530

    step are complate

--- src/components/page/layout/step.js
+++ src/components/page/layout/step.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import '../../style/main.css';
 
+
 const Step = () => {
     return (
-        <div>
-            
-        </div>
+        <section class="steps">
+        
+            <div class="box">
+                <img src="images/step-1.jpg" alt=""/>
+                <h3>choose your favorite food</h3>
+            </div>
+            <div class="box">
+                <img src="images/step-2.jpg" alt=""/>
+                <h3>free and fast delivery</h3>
+            </div>
+            <div class="box">
+                <img src="images/step-3.jpg" alt=""/>
+                <h3>easy payments method</h3>
+            </div>
+            <div class="box">
+                <img src="images/step-4.jpg" alt=""/>
+                <h3>and finally, enjoy your food</h3>
+            </div>
+        
+        </section>
     );
 };
 
