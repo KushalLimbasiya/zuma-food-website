943374a03098ba284c189a579dc14ec0bea9d92c
kushal limbasiya <kushallimbasiya@gmail.com>
2023-05-21 21:12:58 +0530

    add contact page backed as php

--- src/components/page/layout/contact.js
+++ src/components/page/layout/contact.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import '../../style/main.css';
 
 const ContactForm = () => {
@@ -9,42 +10,54 @@ const ContactForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Name:', name);
-    console.log('Email:', email);
-    console.log('Subject:', subject);
-    console.log('Message:', message);
-    // Add code to send form data to server here
+
+    // Create an object with the form data
+    const formData = {
+      name: name,
+      email: email,
+      subject: subject,
+      message: message
+    };
+
+    const url = 'http://localhost/contact.php';
+    let fData = new FormData();
+    fData.append('name', name);
+    fData.append('email', email);
+    fData.append('subject', subject);
+    fData.append('message', message);
+    axios.post(url, fData).then(response=> alert(response.data)).catch(error=> console.error(error));
+
   };
 
   return (
     <section className='contact' id='Contact'>
-        <div className='section-title'>
-            <span className='title'>Contact Us</span>
-        </div>
-        <div className="contact-form">
+      <div className='section-title'>
+        <span className='title'>Contact Us</span>
+      </div>
+      <div className="contact-form">
         <form onSubmit={handleSubmit}>
-            <div className="form-group">
+          <div className="form-group">
             <label htmlFor="name">Name:</label>
             <input type="text" id="name" name="name" value={name} onChange={(e) => setName(e.target.value)} required />
-            </div>
-            <div className="form-group">
+          </div>
+          <div className="form-group">
             <label htmlFor="email">Email:</label>
             <input type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-            </div>
-            <div className="form-group">
+          </div>
+          <div className="form-group">
             <label htmlFor="subject">Subject:</label>
             <input type="text" id="subject" name="subject" value={subject} onChange={(e) => setSubject(e.target.value)} required />
-            </div>
-            <div className="form-group">
+          </div>
+          <div className="form-group">
             <label htmlFor="message">Message:</label>
             <textarea id="message" name="message" value={message} onChange={(e) => setMessage(e.target.value)} required />
-            </div>
-            <div className='form-img'>
+          </div>
+          <div className='form-img'>
 
-            </div>
-            <button type="submit">Submit</button>
+          </div>
+          <button onSubmit={handleSubmit} type="submit">Submit</button>
         </form>
-        </div>
+      </div>
     </section>
   );
 };
